d819d221210fbfac053a10c74d9b2121379c3da9
Alve Larsson <larsalvelarsson@gmail.com>
2021-11-11 21:19:25 +0100

    format

--- Assets/Editor/MoNA/SendToPlayground.cs
+++ Assets/Editor/MoNA/SendToPlayground.cs
@@ -6,25 +6,25 @@ using UnityToolbarExtender;
 namespace MoNA
 {
     [InitializeOnLoad]
-	public class SceneSwitchLeftButton
-	{
-		static SceneSwitchLeftButton()
-		{
-			ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
-		}
+    public class SendToPlayground
+    {
+        static SendToPlayground()
+        {
+            ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
+        }
 
-		static void OnToolbarGUI()
-		{
-			GUILayout.FlexibleSpace();
+        static void OnToolbarGUI()
+        {
+            GUILayout.FlexibleSpace();
 
             GUI.skin.button.fontSize = 13;
-			if(GUILayout.Button(new GUIContent("▶️ Playground", "Build and open playground")))
-			{
+            if (GUILayout.Button(new GUIContent("▶️ Playground", "Build and open playground")))
+            {
                 Helpers.UpsertExportsDirectory();
                 BuildPipeline.BuildAssetBundles(Constants.PlaygroundDirectory, BuildAssetBundleOptions.None, BuildTarget.WebGL);
                 Helpers.OpenDirectory(Constants.ExportsDirectory);
                 Application.OpenURL("https://www.mona.gallery/playground");
-			}
-		}
-	}
+            }
+        }
+    }
 }
