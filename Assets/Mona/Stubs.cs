244e53989813ef42703dbef64ccf290ee9ac4e30
alvelarsson <larsalvelarsson@gmail.com>
2022-01-03 01:26:55 +0100

    Squeeze details

---
+++ Assets/Mona/Stubs.cs
@@ -0,0 +1,12 @@
+// Stubs
+using UnityEngine;
+
+namespace Mona{
+
+    public partial class MonaReactor
+    {
+        public void ReactorAwake() {}
+        public void ReactorOnTriggerEnter(Collider eventObject) {}
+        public void ReactorOnTriggerExit(Collider eventObject) {}
+    }
+}
