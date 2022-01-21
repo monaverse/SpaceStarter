a3447b0ab420488a72f437bd3b7c027026eab011
Alve Larsson <larsalvelarsson@gmail.com>
2022-01-21 19:56:24 +0100

    Fix QAToolbar

--- Assets/Mona/QualityAssurance/QAToolbar.cs
+++ Assets/Mona/QualityAssurance/QAToolbar.cs
@@ -17,22 +17,25 @@ namespace Mona
         {
             GUILayout.FlexibleSpace();
 
-            if (QualityAssurance.ErrorCodes != null)
-            {
-                GUI.skin.button.fontSize = 12;
-                GUI.skin.button.fixedHeight = 22;
-                GUI.skin.button.border = new RectOffset(0, 0, 0, 0);
-                GUI.skin.button.margin = new RectOffset(0, 0, 0, 0);
-                GUI.skin.button.padding = new RectOffset(10, 10, 4, 4);
-                GUI.skin.button.alignment = TextAnchor.MiddleCenter;
-                GUI.color = Color.white * 0.75f;
-                GUI.contentColor = Color.red * 20.19f;
+            GUI.skin.button.fontSize = 12;
+            GUI.skin.button.fixedHeight = 22;
+            GUI.skin.button.border = new RectOffset(0, 0, 0, 0);
+            GUI.skin.button.margin = new RectOffset(0, 0, 0, 0);
+            GUI.skin.button.padding = new RectOffset(10, 10, 4, 4);
+            GUI.skin.button.alignment = TextAnchor.MiddleCenter;
+            GUI.color = Color.white * 0.75f;
 
-                if (GUILayout.Button(new GUIContent("▲ Quality Assurance", "QA")))
-                {
-                    // Open the QA window
-                    QAEditor.Init();
-                }
+            if (QualityAssurance.ErrorCodes != null && QualityAssurance.ErrorCodes.Count != 0)
+            {
+                GUI.contentColor = Color.red * 20.19f;
+            }else{
+                GUI.contentColor = Color.white * 1.2f;
+            }
+
+            if (GUILayout.Button(new GUIContent("▲ Quality Assurance", "QA")))
+            {
+                // Open the QA window
+                QAEditor.Init();
             }
             GUI.color = Color.white;
         }
